00da8ef5ada1900e9b56d805f6be5ad60669de25
gl626qf <baritonhorn@gmail.com>
2023-09-15 16:48:52 +0200

    particle

--- 15092023 - ParticleInBox/operators/linear_algebra_operators.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def operator_trace(op):
-    return np.trace(op)
-
-def conjugate_operator(op):
-    return op.dag()
--- 15092023 - ParticleInBoxC#/operators/BoxHamilton.cs
+++ 15092023 - ParticleInBoxC#/operators/BoxHamilton.cs
@@ -1,1 +1,18 @@
 using system;
+using static system.Console;
+using static System.Math;
+
+
+
+class BoxHamilton{
+
+
+
+    public static void main(){
+
+        Writeline("Hello World!");
+
+    }
+}
+
+
